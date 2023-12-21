6fbfa5e0e97f8fdb44949e3b626c9c5bf04702e3
oribulgogi <hn01184@gmail.com>
2023-12-21 14:37:38 +0900

    ds

---
+++ algorithmSolv/src/BOJ1463.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+/*
+* DP 단계
+* 1. 테이블 정의
+* 2. 점화식 찾기
+* 3. 초기값 정하기
+*
+* 1. D[i] = 정수가 i 를 1로 만들때 연산을 하는 횟수의 최솟값
+*
+* 2. D[12] = ?
+* 3으로 나누거나 ( D[]
+* */
+public class BOJ1463 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(bf.readLine());
+        madeOne(X);
+    }
+    static void madeOne(int X){
+        int dp[] =new int[X];
+        //dp [] ->
+        while()
+    }
+}
