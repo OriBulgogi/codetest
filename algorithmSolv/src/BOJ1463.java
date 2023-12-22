f2b1d4e20751c07665888e0e9b68eaf0e3412c1d
oribulgogi <hn01184@gmail.com>
2023-12-22 21:50:40 +0900

    1463 sol

--- algorithmSolv/src/BOJ1463.java
+++ algorithmSolv/src/BOJ1463.java
@@ -14,14 +14,20 @@ import java.util.*;
 * */
 public class BOJ1463 {
     public static void main(String[] args) throws IOException{
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(bf.readLine());
-        madeOne(X);
-    }
-    static void madeOne(int X){
-        int dp[] =new int[X];
-        //dp [] ->
-        while()
+        int dp[] =new int[X+1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i=2; i<=X; i++){
+            dp[i] = dp[i-1] +1;
+            if(i %2 == 0)
+                dp[i] = Math.min(dp[i], dp[i/2] +1 );
+            if(i %3 == 0)
+                dp[i] = Math.min(dp[i], dp[i/3] +1 );
+        }
+        System.out.println(dp[X]);
     }
 }
