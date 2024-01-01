79eeb79ebfec3950930e8ee86e7cab1a77633f20
oribulgogi <hn01184@gmail.com>
2024-01-01 23:28:05 +0900

    108116 trans HashMap

--- algorithmSolv/src/BOJ108116.java
+++ algorithmSolv/src/BOJ108116.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class BOJ108116 {
@@ -9,32 +11,26 @@ public class BOJ108116 {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] Narr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<N; i++){
-            Narr[i] = Integer.parseInt(st.nextToken());
-        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
+        for(int i=0; i< N; i++){
+            int key = Integer.parseInt(st.nextToken());
+
+            map.put(key, map.getOrDefault(key,0)+1); //key에 해당하는값 반환, 없으면 default 반환
+
+        }
         int M = Integer.parseInt(br.readLine());
-        StringTokenizer Mst = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<M; i++){
-            int cnt=0;
-            int target = Integer.parseInt(Mst.nextToken());
-            for(int j=0; j<N; j++){
-                if(target == Narr[j]){
-                    cnt ++;
-                }
-            }
-            sb.append(cnt+" ");
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(key,0)).append(' ');
         }
+
         System.out.println(sb);
     }
 
-    static void binarySearch(){
-        for(){
-
-        }
-    }
 }
