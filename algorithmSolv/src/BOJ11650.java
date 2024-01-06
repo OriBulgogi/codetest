6bc4b25a63c5717f2c8a9dec1e4c1927f74c879e
oribulgogi <hn01184@gmail.com>
2024-01-06 23:18:58 +0900

    class 2 all clear

--- algorithmSolv/src/BOJ11650.java
+++ algorithmSolv/src/BOJ11650.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class BOJ11650 {
@@ -11,10 +12,27 @@ public class BOJ11650 {
         int N = Integer.parseInt(bf.readLine());
         int[][] arr = new int[N][2];
 
-        for(int i=0; i<N; i++){
+        for(int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
+
         }
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]){
+                return e1[1] - e2[1];
+            }else{
+                return e1[0] - e2[0];
+            }
+        });
+        StringBuilder st = new StringBuilder();
+        for(int i=0; i<N; i++){
+            st.append(arr[i][0]+" "+arr[i][1]+"\n");
+        }
+
+        System.out.println(st);
+
+
+        //from natrang
     }
 }
