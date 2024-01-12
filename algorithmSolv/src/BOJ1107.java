5c0d32e41aabf02561b23c11d2c672917c724d3e
oribulgogi <hn01184@gmail.com>
2024-01-12 18:54:20 +0900

    1107 solv

--- algorithmSolv/src/BOJ1107.java
+++ algorithmSolv/src/BOJ1107.java
@@ -8,13 +8,31 @@ public class BOJ1107 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int target = Integer.parseInt(bf.readLine());
         int brokenNum =Integer.parseInt(bf.readLine());
-        int[] brokenArr = new int[brokenNum];
+        boolean[] brokenArr = new boolean[10];
+
+        int result = Math.abs(target - 100); //초기값 설정
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         for(int i=0; i<brokenNum; i++){
-            brokenArr[i]  = Integer.parseInt(st.nextToken());
+            brokenArr[Integer.parseInt(st.nextToken())] = true;
         }
+        for(int i =0; i<=999999; i++){
+            String str = String.valueOf(i);
+            int len = str.length();
 
+            boolean isBreak = false;
+            for(int j =0; j<len; j++){
+                if(brokenArr[str.charAt(j) - '0']) {// 고장난 버튼을 누르는 경우
+                    isBreak = true;
+                    break;
+                }
+            }
+            if(!isBreak){
+                int min = Math.abs(target -i) +len;
+                result = Math.min(min, result);
+            }
+        }
+        System.out.println(result);
 
     }
 }
