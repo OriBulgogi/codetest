17ecd37c0ee6ebe846f1348dc01fbcf66fe0aab8
oribulgogi <hn01184@gmail.com>
2024-01-24 22:54:58 +0900

    7569 solv

--- algorithmSolv/src/BOJ7569.java
+++ algorithmSolv/src/BOJ7569.java
@@ -9,10 +9,11 @@ import java.util.StringTokenizer;
 public class BOJ7569 {
     static Queue<int[]> qu = new LinkedList<>();
     static int[][][] Box;
-    static int X[] = {-1, 0, 1, 0};
-    static int Y[] = {0, 1, 0, -1};
-    static int Z[] = {-1, 0, 1};
+    static int X[] = { 0, 0, 0, 0, -1, 1 };
+    static int Y[] = { 0, 0, -1, 1, 0, 0 };
+    static int Z[] = { -1, 1, 0, 0, 0, 0 };
     static int N, M, H;
+    static int greenTomato =0;
     static Boolean allDone = false;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -21,17 +22,20 @@ public class BOJ7569 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
+
         Box = new int[H][M][N];
+
         for(int i=0; i<H; i++){
             for(int j=0; j<M; j++){
                 st = new StringTokenizer(bf.readLine());
                 for(int k=0; k<N; k++){
                     Box[i][j][k] = Integer.parseInt(st.nextToken());
+
                     if(Box[i][j][k] == 1){
                         qu.add(new int[] {i,j,k});
-                    }
-                    if(Box[i][j][k] == 0){
+                    }else if(Box[i][j][k] == 0){
                         allDone = true;
+                        greenTomato++;
                     }
                 }
             }
@@ -44,41 +48,28 @@ public class BOJ7569 {
         }
 
         int cnt =0;
-        while (!qu.isEmpty()){
+        while (greenTomato > 0 && !qu.isEmpty()){
             int currentQueueSize = qu.size();
             for(int r = 0; r<currentQueueSize; r++){
                 int[] currentQueue = qu.poll();
 
-                for(int layer=0; layer <=2; layer++) {
-                    int dz = currentQueue[0] + Z[layer];
-
-                    for (int i = 0; i < 4; i++) {
-                        int dx = currentQueue[2] + X[i];
+                    for (int i = 0; i < 6; i++) {
+                        int dz = currentQueue[0] + Z[i];
                         int dy = currentQueue[1] + Y[i];
+                        int dx = currentQueue[2] + X[i];
 
-                        if (dx > -1 && dy > -1 && dz > -1 && dx < N && dy < M &&  dz < H) {
-                            if (Box[dz][dy][dx] == 0) {
-                                Box[dz][dy][dx] = 1;
-                                qu.add(new int[]{dz, dy, dx});
-                            }
-                        }
+                        if (dx < 0 || dy < 0  || dz <0 || dx >= N || dy >= M ||  dz >= H)
+                            continue;
+                        if (Box[dz][dy][dx] != 0)
+                            continue;
+                        greenTomato--;
+                        Box[dz][dy][dx] = 1;
+                        qu.add(new int[]{dz, dy, dx});
                     }
                 }
-            }
-            System.out.println(Arrays.deepToString(Box));
-            cnt ++;
+            cnt++;
         }
 
-        for(int i=0; i<H; i++){
-            for(int j=0; j<M; j++){
-                for(int k=0; k<N; k++){
-                    if(Box[i][j][k] == 0){
-                        return -1;
-                    }
-                }
-            }
-        }
-
-        return cnt;
+        return greenTomato == 0 ? cnt : -1;
     }
 }
