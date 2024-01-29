75b3c244825dd6dedfbb30f9bfa0f4e2a0b98e5a
oribulgogi <hn01184@gmail.com>
2024-01-29 23:50:19 +0900

    10026 solv-dfs

--- algorithmSolv/src/BOJ11724.java
+++ algorithmSolv/src/BOJ11724.java
@@ -6,15 +6,18 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ11724 {
+    static int[][] edgeArr;
+    static boolean[] nodeArr;
+    static int Node, Edge;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int Node = Integer.parseInt(st.nextToken());
-        int Edge = Integer.parseInt(st.nextToken());
+        Node = Integer.parseInt(st.nextToken());
+        Edge = Integer.parseInt(st.nextToken());
 
+        edgeArr = new int[Node+1][Node+1];
+        nodeArr = new boolean[Node+1];
 
-
-        int[][] edgeArr = new int[Edge][2];
         for (int i=0; i<Edge; i++){
             st = new StringTokenizer(bf.readLine());
             int A = Integer.parseInt(st.nextToken());
@@ -23,17 +26,26 @@ public class BOJ11724 {
             edgeArr[A][B] = edgeArr[B][A]= 1;
 
         }
+
+        int count =0;
+        for(int i=1; i<=Node;i++){
+            if(nodeArr[i] == false){
+                DFS(i);
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
-    static int findConn(int[][] edgeArr){
-        Queue<int[]> qu = new LinkedList<>();
-
-        while (!qu.isEmpty()){
-            int nu = qu.poll();
-
-
+    static void DFS(int value){
+        if(nodeArr[value] == true){
+            return;
         }
-
-        return 0;
+        nodeArr[value] = true;
+        for(int i=1; i<=Node; i++){
+            if(edgeArr[value][i] ==1){
+                DFS(i);
+            }
+        }
     }
 }
