53ca5abe3937b7963fecabba182a1fda15e06243
Oribulgogi <hn01184@gmail.com>
2024-03-23 19:14:15 +0900

    2178 solv

--- algorithmSolv/src/BOJ10809.java
+++ algorithmSolv/src/BOJ10809.java
@@ -12,14 +12,16 @@ public class BOJ10809 {
         int alphabet[] = new int[26];
         Arrays.fill(alphabet, -1);
 
-        for(int i=0; i<S.length(); i++){
+        for(int i=0; i<S.length(); i++){  //입력 문자 각각의 알파뱃 위치 생성
             char ch = S.charAt(i);
 
-            if(alphabet[ch- 'a'] == -1){
+            if(alphabet[ch- 'a'] == -1){// a -> index 0
                 alphabet[ch-'a'] =i;
             }
         }
-        for (int i=0; i<alphabet.length; i++){
+
+
+        for (int i=0; i<alphabet.length; i++){  //a~z
             sb.append(alphabet[i]+" ");
         }
         System.out.println(sb);
--- algorithmSolv/src/BOJ2178.java
+++ algorithmSolv/src/BOJ2178.java
@@ -2,36 +2,60 @@ import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ2178 {
-    /*
-    *
-    *
-4 6
-101111
-101010
-101011
-111011
-    *
-    * */
+    static int[][] arr;
+    static int[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int N;
+    static int M;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][M];
+        arr = new int[N][M];
+        visited = new int[N][M];
         String str;
 
         for(int i =0; i<N; i++){
-            str = bf.readLine();
+            String S = bf.readLine();
             for (int j=0; j<M; j++){
-                arr[i][j] = str.charAt(j) - '0';
+                arr[i][j] = S.charAt(j) -'0';
             }
         }
+        bfs();
+    }
 
+    static void bfs(){
+        Queue<int[]> qu = new LinkedList<>();
+        qu.add(new int[] {0,0} );
+        visited[0][0] = 1;
+        while (!qu.isEmpty()){
+            int[] currentPosition = qu.poll();
+            int curX = currentPosition[0];
+            int curY = currentPosition[1];
 
+            for(int i =0; i<4; i++){
+                int x = curX+ dx[i];
+                int y = curY+ dy[i];
+                if(x >=0 && y>=0 && x <N && y <M){
+                    if(arr[x][y] == 1 && visited[x][y] == 0){
+                        qu.offer(new int[] {x,y});
+                        visited[x][y] = visited[curX][curY]+1;
+                    }
+                }
+            }
+        }
+        System.out.println(visited[N-1][M-1]);
     }
 }
