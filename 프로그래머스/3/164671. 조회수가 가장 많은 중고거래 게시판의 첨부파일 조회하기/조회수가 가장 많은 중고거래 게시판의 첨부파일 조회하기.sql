6b7ed3e592a82bb48b28703e3db4bd35bd86c119
OriBulgogi <81247213+OriBulgogi@users.noreply.github.com>
2025-02-10 17:30:47 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT concat('/home/grep/src/', B.BOARD_ID,'/', B.FILE_ID, B.FILE_NAME, B.FILE_EXT) as FILE_PATH
+from USED_GOODS_BOARD A
+left join USED_GOODS_FILE B ON A.BOARD_ID = B.BOARD_ID
+where `VIEWS` = (select MAX(`VIEWS`) from USED_GOODS_BOARD)
